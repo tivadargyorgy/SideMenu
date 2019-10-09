992de202446458cd9a104743fe4377b705dae82b
Tivadar György <tivadar.gyorgy@supercharge.io>
2019-10-09 22:09:51 +0200

    Remove iOS13 related condition
    It requires iOS13 SDK

--- Pod/Classes/SideMenuPresentationController.swift
+++ Pod/Classes/SideMenuPresentationController.swift
@@ -91,12 +91,7 @@ internal final class SideMenuPresentationController {
 
         guard let statusBarView = statusBarView else { return }
         let statusBarOffset = containerView.frame.size.height - presentedViewController.view.bounds.height
-        var statusBarFrame: CGRect
-        if #available(iOS 13.0, *) {
-            statusBarFrame = containerView.window?.windowScene?.statusBarManager?.statusBarFrame ?? .zero
-        } else {
-            statusBarFrame = UIApplication.shared.statusBarFrame
-        }
+        var statusBarFrame = UIApplication.shared.statusBarFrame
 
         // For in-call status bar, height is normally 40, which overlaps view. Instead, calculate height difference
         // of view and set height to fill in remaining space.
